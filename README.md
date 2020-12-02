f6f156310bb1f79a4fd1f7ba9af4cfa7552632a3
pvrtykv <jotpe2307@gmail.com>
2020-12-02 20:55:09 +0100

    added new functionalities to interfaces

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # TravelAgency
+This is a simple console application that allows user to perform operations on travel_agency database. TravelAgency is a databases course project. It was created with Python 3.8 and PyMySQL, sys modules.
+## Description
+TravelAgency allows you to access travel_agency database as a client or as an employee. Client and employee interfaces have different functionalities.
+### SQL queries 
+Examples of SQL queries that were used in the app.
+1. Select all trip offers where trip price is less than 2000 and hotel standard is at least 3-star.
+```sql
+  SELECT o.name,  l.city_name AS city, c.name AS country,
+  h.name as hotel, h.stars, o.trip_price FROM offer o 
+  INNER JOIN location l ON o.location_id = l.id 
+  INNER JOIN country ON l.country_id = c.id 
+  INNER JOIN hotel h ON o.hotel_id = h.id
+  WHERE o.trip_price < 2000 AND h.stars IN ("3","4","5");
+ ```
+2. Add new client
+```sql
+INSERT INTO client (id, full_name, phone_number)
+VALUES ('{id_}', '{full_name}', '{phone_number}');
+```
+
+### Database
+![ERD](travel_agency_erd.png)
+*travel_agency database*
--- interfaces.py
+++ interfaces.py
@@ -1,24 +1,74 @@
 import utils
+import sys
+
+
+def main_menu():
+    choice = input("Witaj w systemie obsługi Biura Podróży!\n"
+                   "Wpisz numer interesującej cię opcji:\n"
+                   "1. Jestem klientem\n"
+                   "2. Jestem pracownikiem\n"
+                   "3. Wyjście z programu \n")
+
+    if choice == "1":
+        client_interface()
+    elif choice == "2":
+        employee_interface()
+    elif choice == "3":
+        sys.exit()
 
 
 def client_interface():
     choice = input("Wybierz:\n"
-                   "1. Chcę wyświetlić dostępne wycieczki\n"
+                   "1. Wyświetl dostępne wycieczki\n"
+                   "2. Wyświetl moje dane\n"
+                   "3. Powrót do menu głównego\n"
                    )
-    if (choice == "1"):
+    if choice == "1":
         utils.show_offers()
-
-
-        # for row in rows:
-        #   print(f'{row[0]:<8} {row[1]:<15} {row[2]:>10}')
+    elif choice == "2":
+        id_ = input("Podaj swój numer identyfikacyjny:\n")
+        utils.show_your_info(id_)
+    elif choice == "3":
+        main_menu()
+    print()
+    client_interface()
 
 
 def employee_interface():
     choice = input("Wybierz:\n"
-                   "1. Chce wyświetlić dostępne wycieczki \n"
-                   "2. Chcę wyświetlić dane o klientach \n")
+                   "1. Zarządzaj wycieczkami \n"
+                   "2. Zarządzaj danymi klientów \n"
+                   "3. Powrót do menu głównego\n")
 
     if choice == "1":
+        manage_offers()
+    elif choice == "2":
+        manage_client_info()
+    elif choice == "3":
+        main_menu()
+    employee_interface()
+
+
+def manage_offers():
+    choice = input("Wybierz: \n"
+                   "1. Wyświetl dostępne oferty \n"
+                   "2. Usuń ofertę\n")
+    if choice == "1":
         utils.show_offers()
-    elif (choice == "2"):
+    elif choice == "2":
+        utils.delete_offer()
+
+
+def manage_client_info():
+    choice = input("Wybierz:\n"
+                   "1. Wyświetl dane klientów\n"
+                   "2. Dodaj nowego klienta\n"
+                   "3. Usuń klienta \n")
+    if choice == "1":
         utils.show_client_info()
+    elif choice == "2":
+        utils.add_client()
+    elif choice == "3":
+        utils.delete_client()
+
+
